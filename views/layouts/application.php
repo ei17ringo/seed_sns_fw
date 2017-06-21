e5c8e4be2c2eadb120cf877ebd86d089649671de
ei17ringo <ei17ringo@gmail.com>
2017-06-22 07:37:31 +0800

    init commit

---
+++ views/layouts/application.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Seed SNS Framework</title>
+</head>
+<body>
+<?php
+	echo 'hello,app!';
+
+	include('views/'.$resouce.'/'.$action.'.php');
+?>
+</body>
+</html>
